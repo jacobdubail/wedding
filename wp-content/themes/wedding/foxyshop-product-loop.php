89c7e8221724b991f6a2eb54044d02c35fd86b22
Jacob Dubail <jacob@jacobdubail.com>
2013-01-09 19:13:02 -0800

    commiting from subl

--- wp-content/themes/wedding/foxyshop-product-loop.php
+++ wp-content/themes/wedding/foxyshop-product-loop.php
@@ -20,7 +20,7 @@ global $product, $post;
 $product = foxyshop_setup_product();
 if (!$product['hide_product']) {
 
-	echo '<li class="foxyshop_product_box col-1-2 item">';
+	echo '<li class="foxyshop_product_box item">';
 
 	//Show Image on Left
 	echo '<div class="foxyshop_product_image">';
