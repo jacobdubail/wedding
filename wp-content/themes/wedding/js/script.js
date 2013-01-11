76802cb91cb64499bc15a429e1d6b8467182f3e8
Jacob Dubail <jacob@jacobdubail.com>
2013-01-10 21:17:15 -0800

    commiting from subl

--- wp-content/themes/wedding/js/script.js
+++ wp-content/themes/wedding/js/script.js
@@ -6,11 +6,11 @@ var $bg         = $("#bg"),
       var w_w   = $(window).width();
 
 
-      if ( w_w <= 480 ) {
+      if ( w_w <= 900 ) {
         return 1;
-      } else if ( w_w <= 768 && w_w > 480 ) {
-        return 2;
-      } else if ( w_w <= 1024 && w_w > 768) {
+//      } else if ( w_w <= 900 && w_w > 600 ) {
+ //       return 1;
+      } else if ( w_w <= 1024 && w_w > 900) {
         return 2;
       } else if ( w_w <= 1200 && w_w > 1024) {
         return 3;
