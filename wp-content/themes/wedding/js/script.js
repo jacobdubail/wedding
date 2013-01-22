05ae5cafb2e6ba924de62b27aa76ca5b54240ee0
Jacob Dubail <jacobdubail@gmail.com>
2013-01-22 15:51:47 -0800

    commiting from subl

--- wp-content/themes/wedding/js/script.js
+++ wp-content/themes/wedding/js/script.js
@@ -62,8 +62,8 @@ $('#filters').on( 'click', 'a', function(e) {
     $container.isotope({ filter: selector });
 
     if ( $("body").hasClass('single-foxyshop_product') ) {
-      window.location.pathname = "registry";
-      window.location.hash     = selector;
+      window.location.pathname = "registry#" + selector.substring(1);
+      window.location.hash     = selector.substring(1);
     }
 
     $this
