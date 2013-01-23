66a5759b01fa4d92f46271a5135607e2173dae49
Jacob Dubail <jacobdubail@gmail.com>
2013-01-22 16:26:52 -0800

    commiting from subl

--- wp-content/themes/wedding/foxyshop-single-product.php
+++ wp-content/themes/wedding/foxyshop-single-product.php
@@ -8,40 +8,30 @@ This will allow you to upgrade FoxyShop without breaking your customizations. Mo
 <?php get_header(); ?>
 
 <?php foxyshop_include('header'); ?>
-<div id="foxyshop_container">
+<div id="foxyshop_container" class="grid">
+
 <?php
-while (have_posts()) : the_post();
+	while (have_posts()) : the_post();
 
 	//Initialize Product
 	global $product;
 	$product = foxyshop_setup_product();
 
-
-	//This is for testing to see what is included in the $product array
-	//print_r($product);
-
 	//Initialize Form
 	foxyshop_start_form();
 
-	//Write Breadcrumbs
-	//foxyshop_breadcrumbs(" &raquo; ", "&laquo; Back to Products");
-
-
-	//Shows Main Image and Optional Slideshow
-	//Available Built-in Options: prettyPhoto (lightbox), cloud-zoom (inline zooming), or colorbox (native FoxyCart lightbox)
-	//Second arg writes css and js includes on page
-	//If you want to make more customizations, you can grab the code from helperfunctions.php line ~650 and paste here
-	//-------------------------------------------------------------------------------------------------------------------------
 	foxyshop_build_image_slideshow("prettyPhoto", true);
 	//foxyshop_build_image_slideshow("cloud-zoom", true);
 	//foxyshop_build_image_slideshow("colorbox", true); //only recommended for 0.7.2+
 
+?>
 
-	//Main Product Information Area
-	echo '<div class="foxyshop_product_info">';
-	//edit_post_link('<img src="' . FOXYSHOP_DIR . '/images/editicon.png" alt="Edit Product" width="16" height="16" />','<span class="foxyshop_edit_product">','</span>');
-	echo '<h2>' . apply_filters('the_title', $product['name']) . '</h2>';
 
+	<div class="foxyshop_product_info">
+
+		<h2><?php echo apply_filters('the_title', $product['name']); ?></h2>
+
+<?php
 	//Show a sale tag if the product is on sale
 	//if (foxyshop_is_on_sale()) echo '<p class="sale-product">SALE!</p>';
 
@@ -53,11 +43,7 @@ while (have_posts()) : the_post();
 
 
 	//Show Variations (showQuantity: 0 = Do Not Show Qty, 1 = Show Before Variations, 2 = Show Below Variations)
-	//If Qty is turned off on product, Qty box will not be shown at all
 	foxyshop_product_variations(2);
-
-	//(style) clear floats before the submit button
-	echo '<div class="clr"></div>';
 
 	//Check Inventory Levels and Display Status (last variable allows backordering of out of stock items)
 	foxyshop_inventory_management("There are only %c item%s left in stock.", "Item is not in stock.", false);
@@ -70,7 +56,7 @@ while (have_posts()) : the_post();
 
 	//Shows the Price (includes sale price if applicable)
 	echo '<div id="foxyshop_main_price">';
-	foxyshop_price();
+		foxyshop_price();
 	echo '</div>';
 
 	//Shows any related products
@@ -78,12 +64,6 @@ while (have_posts()) : the_post();
 
 
 	//Custom Code Can Go Here
-
-
-
-
-
-
 
 
 	//Ends the form
@@ -94,7 +74,6 @@ while (have_posts()) : the_post();
 endwhile;
 ?>
 
-	<div class="clr"></div>
 </div>
 
 <?php foxyshop_include('footer'); ?>
